f1f7d16d90553557944f3c13b9de587d5433f9e6
CocoHara <cormacpatrickohara@gmail.com>
2019-06-12 20:17:05 +0100

    First commit, Basic Cat Application

---
+++ app/src/main/java/com/example/cats/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.cats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MainActivity extends AppCompatActivity
+{
+    List<String> cats = new ArrayList<String>();
+    private int imageIndex = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cats.add("cat1");
+        cats.add("cat2");
+        cats.add("cat3");
+        cats.add("cat4");
+        cats.add("cat5");
+
+        ImageView catImage = (ImageView) findViewById(R.id.catImage);
+
+        int imageResource = getResources().getIdentifier("@drawable/"+cats.get(0), null, this.getPackageName());
+        catImage.setImageResource(imageResource);
+    }
+
+    public void nextImage(View view)
+    {
+        imageIndex++;
+        ImageView catImage = (ImageView) findViewById(R.id.catImage);
+
+        if (imageIndex < cats.size()) {
+            int imageResource = getResources().getIdentifier("@drawable/"+cats.get(imageIndex), null, this.getPackageName());
+            catImage.setImageResource(imageResource);
+        }
+        else {
+            imageIndex = 0;
+            int imageResource = getResources().getIdentifier("@drawable/"+cats.get(imageIndex), null, this.getPackageName());
+            catImage.setImageResource(imageResource);
+        }
+    }
+
+    public void previousImage(View view)
+    {
+        imageIndex--;
+        ImageView catImage = (ImageView) findViewById(R.id.catImage);
+
+        if (imageIndex > 0) {
+            int imageResource = getResources().getIdentifier("@drawable/"+cats.get(imageIndex), null, this.getPackageName());
+            catImage.setImageResource(imageResource);
+        }
+        else {
+            imageIndex = 0;
+            int imageResource = getResources().getIdentifier("@drawable/"+cats.get(imageIndex), null, this.getPackageName());
+            catImage.setImageResource(imageResource);
+        }
+    }
+
+    public void exitApp (View view)
+    {
+        finish();
+        System.exit(0);
+    }
+}
